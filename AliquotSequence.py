1488e1b473d840c25508f02b9f36136493b0a955
decimatedTomato <greentomatopower@gmail.com>
2023-06-09 17:50:14 +0200

    The idea of this project is to find all kinds of mathematical properties of input numbers

---
+++ AliquotSequence.py
@@ -0,0 +1,9 @@
+# Aliquot sequence code
+"""
+Sources:
+https://www.youtube.com/watch?v=Cw0IYZuo3HU&ab_channel=ComboClass
+https://en.wikipedia.org/wiki/Aliquot_sequence
+https://www.geeksforgeeks.org/aliquot-sequence/
+"""
+# Before I start coding I would like to make a proper project folder and get a pipenv virtual environment
+#
